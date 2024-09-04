b5b43f1a9298602b2fcf76fa3da0d777f656225b
KevinDGutierrez <kgutierrez-2023455@kinal.edu.gt>
2024-09-04 11:35:50 -0600

    MarvelApp con fondo

--- src/components/SearchCharacters.jsx
+++ src/components/SearchCharacters.jsx
@@ -13,15 +13,8 @@ export const SearchCharacter = ({ searchCharacter }) => {
             <nav className="navbar bg-body-tertiary">
                 <div className="container-fluid">
                     <form className="d-flex" role="search" onSubmit={handleSubmit}>
-                        <input 
-                            className="form-control me-2 border border-warning text-warning" 
-                            type="search" 
-                            placeholder="Search" 
-                            aria-label="Search"
-                            value={query}
-                            onChange={(e) => setQuery(e.target.value)}
-                        />
-                        <button className="btn btn-outline-warning border border-warning text-dark" type="submit">Buscar</button>
+                        <input className="form-control me-2 border border-warning text-warning" type="search" placeholder="Search" aria-label="Search"value={query}onChange={(e) => setQuery(e.target.value)}/>
+                        <button className="btn btn-outline-warning border border-warning text-warning" type="submit">Buscar</button>
                     </form>
                 </div>
             </nav>
--- src/services/characters.jsx
+++ src/services/characters.jsx
@@ -2,6 +2,7 @@ import CryptoJS from 'crypto-js';
 
 
 export const reqCharacters = async (name = '', offset = 0) => {
+  
   const timeStamp = new Date().getTime();
   const PRIVATE_API_KEY = '69458f5dfd98235e7719b7e145c3b1d56db567c4';
   const PUBLIC_API_KEY = 'e70b3b1a1f2c828c3a7828f064243f15';
